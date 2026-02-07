26e0559b49c4f1b9ebf9ef9d65c29ecabff76369
thesecretstrider <thesecretstrider@gmail.com>
2026-02-07 10:37:08 +1000

    Save Game 2

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
 import net.minecraft.client.Minecraft;
@@ -15,24 +16,46 @@ import java.nio.file.Path;
 public class ExampleModClient implements ClientModInitializer {
 	private static final String DEFAULT_WELCOME_TEXT = "Welcome sacred artist, you awaken in the world of Cradle";
 	private static final Path WELCOME_TEXT_PATH = Path.of("modid-welcome.txt");
+	private static final int WELCOME_MESSAGE_TICKS = 20 * 10;
+
+	private static int welcomeTicksRemaining = 0;
+	private static String welcomeTextCached = DEFAULT_WELCOME_TEXT;
 
 	@Override
 	public void onInitializeClient() {
 		ClientPlayConnectionEvents.JOIN.register(new ClientPlayConnectionEvents.Join() {
 			@Override
 			public void onPlayReady(ClientPacketListener handler, PacketSender sender, Minecraft client) {
-				showWelcomeMessage(client);
+				startWelcomeMessage(client);
+			}
+		});
+
+		ClientPlayConnectionEvents.DISCONNECT.register(new ClientPlayConnectionEvents.Disconnect() {
+			@Override
+			public void onPlayDisconnect(ClientPacketListener handler, Minecraft client) {
+				welcomeTicksRemaining = 0;
+			}
+		});
+
+		ClientTickEvents.END_CLIENT_TICK.register(new ClientTickEvents.EndTick() {
+			@Override
+			public void onEndTick(Minecraft client) {
+				if (welcomeTicksRemaining <= 0 || client.player == null) {
+					return;
+				}
+
+				client.player.displayClientMessage(Component.literal(welcomeTextCached), true);
+				welcomeTicksRemaining--;
 			}
 		});
 	}
 
-	private static void showWelcomeMessage(Minecraft client) {
+	private static void startWelcomeMessage(Minecraft client) {
 		client.execute(new Runnable() {
 			@Override
 			public void run() {
-				if (client.player != null) {
-					client.player.displayClientMessage(Component.literal(welcomeText()), true);
-				}
+				welcomeTextCached = welcomeText();
+				welcomeTicksRemaining = WELCOME_MESSAGE_TICKS;
 			}
 		});
 	}
